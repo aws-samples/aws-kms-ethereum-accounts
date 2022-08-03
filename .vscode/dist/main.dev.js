37137767744ec52c55b069c17bb2de55b5764c4e
root <waxmigs2992@outlook.ph>
2022-08-03 11:53:53 +0800

    cheeers

---
+++ .vscode/dist/main.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/// <reference path="/home/ethereum/.vscode/extensions/nur.script-0.2.1/@types/api.global.d.ts" />
+/// <reference path="/home/ethereum/.vscode/extensions/nur.script-0.2.1/@types/vscode.global.d.ts" />
+//  @ts-check
+//  API: https://code.visualstudio.com/api/references/vscode-api
+function activate(_context) {
+  window.showInformationMessage('Hello, World!');
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate: activate,
+  deactivate: deactivate
+};
---
+++ component.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Component extends Vue {
+
+}
+</script>
+<style scoped>
+</style>
